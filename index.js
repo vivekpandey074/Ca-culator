52e258430a554dde42e4b4dc7a0852eafb418d1a
vivek pandey <vivekpande074@gmail.com>
2024-04-08 22:48:44 +0530

    Calc

---
+++ index.js
@@ -0,0 +1,36 @@
+const resultText=document.getElementById("resultText")
+
+
+function inputChar(input){
+    resultText.value+=input;
+}
+
+
+function backspace(){
+    if(resultText.value.length===1 || resultText.value==="Error"  || resultText.value==="Infinity"){
+        resultText.value="";
+        return;
+    }
+    resultText.value=resultText.value.slice(0,-1);
+}
+
+function clearInput(){
+    resultText.value="";
+}
+ 
+
+function calculate(){
+    try{
+const temp=eval(resultText.value).toFixed(3);
+if(temp.slice(-3)==="000"){
+    resultText.value=temp.slice(0,-4);
+    return;
+}
+resultText.value=temp;
+
+
+    }
+    catch(error){
+      resultText.value="Error";
+    }
+}
